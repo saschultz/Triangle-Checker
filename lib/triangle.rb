9d0a90d0fc4f57df74272ad5a920ed11b9ea063f
Sara Schultz and Tanner Eustice <tanner@>
2017-04-24 09:47:39 -0700

    add method to check the type of triangle

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -6,8 +6,21 @@ class Triangle
   end
 
   def triangle_type
-    if((@side1 == @side2) && (@side1 == @side3))
-      true
+    if (@side1 == @side2) && (@side1 == @side3)
+      "Equilateral"
+
+    elsif (@side1 == @side2 && @side1 + @side2 > @side3) || (@side2 == @side3 && @side2 + @side3 > @side1) || (@side1 == @side3 && @side1 + @side3 > @side2)
+      "Isosceles"
+
+    elsif (@side1 + @side2 <= @side3) || (@side2 + @side3 <= @side1) || (@side1 + @side3 <= @side2)
+      if( @side1.class == String || @side2.class == String || @side3.class == String)
+        "That is NOT valid input. Please use positive numbers only"
+      else
+        "Not a triangle"
+      end
+      
+    elsif (@side1 != @side2 && @side1 + @side2 > @side3 ) || (@side2 != @side3 && @side2 + @side3 > @side1 ) || (@side1 != @side3 && @side1 + @side3 > @side2)
+      "Scalene"
     end
   end
 end
--- spec/triangle_spec.rb
+++ spec/triangle_spec.rb
@@ -3,9 +3,29 @@ require 'triangle'
 
 describe(Triangle) do
   describe("#triangle_type") do
-    it("returns true if the triangle is equilateral") do
+    it("returns 'Equilateral' if the triangle is Equilateral") do
       new_triangle = Triangle.new(2,2,2)
-      expect(new_triangle.triangle_type).to(eq(true))
+      expect(new_triangle.triangle_type).to(eq("Equilateral"))
+    end
+
+    it("returns 'Isosceles' if the triangle is Isosceles") do
+      new_triangle = Triangle.new(2,4,4)
+      expect(new_triangle.triangle_type).to(eq("Isosceles"))
+    end
+
+    it("returns 'Scalene' if the triangle is Scalene") do
+      new_triangle = Triangle.new(2,5,6)
+      expect(new_triangle.triangle_type).to(eq("Scalene"))
+    end
+
+    it("returns 'Not a triangle' if the triangle is Not a triangle") do
+      new_triangle = Triangle.new(2,5,25)
+      expect(new_triangle.triangle_type).to(eq("Not a triangle"))
+    end
+
+    it("checks if the method is called on numbers") do
+      new_triangle = Triangle.new("apple", "burrito", "Niklas")
+      expect(new_triangle.triangle_type).to(eq("That is NOT valid input. Please use positive numbers only"))
     end
   end
 end
